7588a4f82b12d2b8367f8724e79b582cec6069f4
figmentbml <beth.jaswa@gmail.com>
2014-10-14 11:00:51 -0600

    working verses

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,13 @@
 class PagesController < ApplicationController
+  def verse2
+    render "verse2"
+    end
 
+  def verse3
+    render "verse3"
+    end
+
+  def verse4
+    render "verse4"
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root to: "pages#index"
+  get "/verse2" => "pages#verse2", as: :verse2
+  get "/verse3" => "pages#verse3", as: :verse3
+  get "/verse4" => "pages#verse4", as: :verse4
+  get "/" => "pages#index", as: :index
 end
